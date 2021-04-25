a5abd0a835e42a39de1bcc368f3eeab08f90ba9d
raszta9999 <raszta9999@wp.pl>
2021-04-25 12:33:13 +0200

    to pass tests

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -23,7 +23,6 @@ namespace WishList
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseRouting().UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -32,6 +31,9 @@ namespace WishList
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+            app.UseRouting();
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+           
         }
     }
 }
